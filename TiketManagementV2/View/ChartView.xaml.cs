3b3e09fe2bfd9d698366c7af43f55e7f2fc2d752
anhnguyen427 <muradavalorvn@gmail.com>
2025-03-02 19:17:15 +0700

    cải tiến chart

--- TiketManagementV2/View/ChartView.xaml.cs
+++ TiketManagementV2/View/ChartView.xaml.cs
@@ -14,29 +14,6 @@ namespace TiketManagementV2.View
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        // These properties match what your XAML is looking for
-        private ChartValues<double> _marketingCosts;
-        public ChartValues<double> MarketingCosts
-        {
-            get => _marketingCosts;
-            set
-            {
-                _marketingCosts = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MarketingCosts)));
-            }
-        }
-
-        private ChartValues<double> _salesCosts;
-        public ChartValues<double> SalesCosts
-        {
-            get => _salesCosts;
-            set
-            {
-                _salesCosts = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SalesCosts)));
-            }
-        }
-
         private ChartValues<double> _hiringSources;
         public ChartValues<double> HiringSources
         {
@@ -48,17 +25,6 @@ namespace TiketManagementV2.View
             }
         }
 
-        // Your existing properties for labels
-        private List<string> _months;
-        public List<string> Months
-        {
-            get => _months;
-            set
-            {
-                _months = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Months)));
-            }
-        }
 
         private List<string> _sourceLabels;
         public List<string> SourceLabels
@@ -80,13 +46,9 @@ namespace TiketManagementV2.View
 
         private void LoadSampleData()
         {
-            // Set data for both charts
-            MarketingCosts = new ChartValues<double> { 10, 20, 15, 25, 30, 18, 22, 28 };
-            SalesCosts = new ChartValues<double> { 8, 15, 12, 20, 25, 15, 18, 22 };
-            HiringSources = new ChartValues<double> { 50, 60, 55, 70, 65, 80, 75, 85 };
+            HiringSources = new ChartValues<double> { 50, 60, 55, 70, 65, 80, 75, 85, 90, 100, 75, 40 };
 
-            Months = new List<string> { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug" };
-            SourceLabels = new List<string> { "Direct", "LinkedIn", "Hired", "Workin", "Instagram", "Referral" };
+            SourceLabels = new List<string> { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
         }
     }
 }
